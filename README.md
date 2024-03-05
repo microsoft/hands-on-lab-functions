cbfbf138fa203829eaec96364beac065120d15ef
Damien Aicheh <38168153+damienaicheh@users.noreply.github.com>
2024-03-05 10:19:37 +0100

    feat: Initial commit
    Co-authored-by: @ikhemissi 
    Co-authored-by: @Justrebl

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project
+# Hands On Lab Functions
 
 > This repo has been populated by an initial template to help get you started. Please
 > make sure to update the content to build a great experience for community-building.
---
+++ terraform/egst.tf
@@ -0,0 +1,7 @@
+resource "azurerm_eventgrid_system_topic" "this" {
+  name                   = format("egst-audio-storage-%s", local.resource_suffix_kebabcase)
+  resource_group_name    = azurerm_resource_group.this.name
+  location               = azurerm_resource_group.this.location
+  source_arm_resource_id = azurerm_storage_account.this.id
+  topic_type             = "Microsoft.Storage.StorageAccounts"
+}
